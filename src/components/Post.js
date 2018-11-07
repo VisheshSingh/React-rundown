bc6bfc2af89019a24d1510145b87919c84d76dca
Vishesh <visheshsingh16@gmail.com>
2018-11-07 15:29:29 -0500

    Mapping dispatch to props

--- src/components/Post.js
+++ src/components/Post.js
@@ -2,11 +2,20 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 
 class Post extends Component {
+  handleClick = () => {
+    this.props.deletePost(this.props.post.id);
+    this.props.history.push("/");
+  };
   render() {
     const post = this.props.post ? (
-      <div className="post">
-        <h4 className="center">{this.props.post.title}</h4>
-        <p>{this.props.post.body}</p>
+      <div className="post card">
+        <div className="card-content">
+          <h4 className="center">{this.props.post.title}</h4>
+          <p>{this.props.post.body}</p>
+          <div className="center" onClick={this.handleClick}>
+            <button className="btn grey white-text">delete post</button>
+          </div>
+        </div>
       </div>
     ) : (
       <div className="center">loading posts...</div>
@@ -20,4 +29,14 @@ const mapStateToProps = (state, ownProps) => {
     post: state.posts.find(post => post.id === id)
   };
 };
-export default connect(mapStateToProps)(Post);
+
+const mapDispatchToProps = dispatch => {
+  return {
+    deletePost: id => dispatch({ type: "DELETE_POST", id: id })
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Post);
